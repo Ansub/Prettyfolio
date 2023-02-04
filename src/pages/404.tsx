fea1646d532bf04067483e697d546e12065eff31
Ansub <khanansub3@gmail.com>
2023-02-04 15:12:16 +0530

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,43 @@
+import { Flex, Box, Button } from "@chakra-ui/react"
+import Layout from "../components/layout"
+import { useRouter } from "next/router"
+import Image from "next/image"
+const NotFound = () => {
+  const router = useRouter()
+  return (
+    <Layout>
+      <Flex
+        direction="column"
+        height="80vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Image
+          src="/images/prettyfolio.svg"
+          height={200}
+          width={200}
+          alt="prettyfolio"
+        />
+        <Box textStyle={{ base: "cal.md", lg: "cal.xl" }} color="theme.orange">
+          Page Not Found!
+        </Box>
+        <Button
+          marginTop="1rem"
+          background="theme.orange"
+          color="white"
+          onClick={() => router.push("/")}
+          textStyle="cal.sm"
+          _hover={{
+            background: "theme.orange",
+            transform: "scale(1.05)",
+            color: "white",
+          }}
+        >
+          Go Back Home
+        </Button>
+      </Flex>
+    </Layout>
+  )
+}
+
+export default NotFound
