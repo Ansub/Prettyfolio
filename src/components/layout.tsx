83a07dc3aebf23136c25f0a67dd3e340cbf70bef
Ansub <khanansub3@gmail.com>
2023-02-08 19:34:44 +0530

    dark mode revert

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
-import { Flex, Box } from "@chakra-ui/react"
+import { ChakraProvider, Flex } from "@chakra-ui/react"
 import Navbar from "./reusable/navbar"
+import Theme from "../theme"
 import "cal-sans"
 import Footer from "./reusable/footer"
 
@@ -9,13 +10,13 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <Box>
+    <ChakraProvider theme={Theme}>
       <Navbar />
       <Flex direction="column" flex="1" minHeight="0" w="full">
         {children}
       </Flex>
       <Footer />
-    </Box>
+    </ChakraProvider>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,8 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import { ChakraProvider, cookieStorageManagerSSR } from "@chakra-ui/react"
-import theme from "../theme"
 
-function MyApp({ Component, pageProps, cookies }: AppProps & { cookies: any }) {
-  const colorModeManager = cookieStorageManagerSSR(cookies)
-  return (
-    <ChakraProvider colorModeManager={colorModeManager} theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
-}
-MyApp.getInitialProps = (data: any) => {
-  const { ctx } = data
-  return {
-    cookies: ctx?.req?.headers?.cookie || "",
-  }
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
